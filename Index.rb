31236494a9d8438b446dc901e825f9d46e915a58
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-01-06 22:44:39 -0800

    complete bubble_sort_by method

--- Index.rb
+++ Index.rb
@@ -16,3 +16,26 @@ def bubble_sort(arr)
 end
    sort=[3, 1, 4, 7]
    p bubble_sort(sort)
+
+
+def bubble_sort_by(arr)
+  len = arr.length
+  loop do
+    swapped =  false
+    (len-1).times do |a|
+      if yield(arr[a], arr[a+1]).positive?
+        temp=arr[a]
+        arr[a]=arr[a+1]
+        arr[a+1]=temp
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  arr
+end
+sort_by = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+   left.length - right.length
+
+end
+p sort_by
