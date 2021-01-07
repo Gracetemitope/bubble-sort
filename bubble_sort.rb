ede90962a43e337a23b24f0faf6ecdf211790551
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-01-06 23:25:19 -0800

    fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,29 +1,32 @@
 def bubble_sort(arr)
   len = arr.length
+
   loop do
     swapped = false
+
     (len - 1).times do |a|
+
       if arr[a] > arr[a + 1]
         temp = arr[a]
         arr[a] = arr[a + 1]
         arr[a + 1] = temp
         swapped = true
-        next
       end
     end
     break unless swapped
   end
   arr
 end
-   sort = [3, 1, 4, 7]
-   p bubble_sort(sort)
-
+sort = [3, 1, 4, 7]
+p bubble_sort(sort)
 
 def bubble_sort_by(arr)
   len = arr.length
+
   loop do
     swapped = false
-    (len-1).times do |a|
+
+    (len - 1).times do |a|
       if yield(arr[a], arr[a + 1]).positive?
         temp = arr[a]
         arr[a] = arr[a + 1]
@@ -35,8 +38,7 @@ def bubble_sort_by(arr)
   end
   arr
 end
-sort_by = bubble_sort_by(["hi","hello","hey"]) do |left,right|
-   left.length - right.length
-
+sort_by = bubble_sort_by(%w[hi, hello, hey]) do |left, right|
+  left.length - right.length
 end
 p sort_by
